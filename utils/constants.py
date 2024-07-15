2cef0326bba61ec6d144fba8edf3206db0450e22
Iri O <oparina.iri@gmail.com>
2024-07-15 19:41:05 +0500

    Added constants for email types

---
+++ utils/constants.py
@@ -0,0 +1,8 @@
+CONFIRM_EMAIL = 'confirm_email'
+PASSWORD_RESTORE = 'password_restore'
+
+
+EMAIL_TYPES = (
+    (CONFIRM_EMAIL, 'Подтверждение адреса электронной почты'),
+    (PASSWORD_RESTORE, 'Восстановление пароля'),
+)
