ddf17886f80a4ecd10fa9821a9543eb1b2a39e32
Iri O <oparina.iri@gmail.com>
2024-07-19 19:30:18 +0500

    Added serializer for Area, Contact, Photo

---
+++ apps/areas/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from areas.models import (
+    Area,
+    Contact,
+    Photo,
+)
+
+
+class ContactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contact
+        fields = [
+            'contact',
+            'contact_type',
+        ]
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Photo
+        fields = [
+            'photo',
+        ]
+
+
+class AreaSerializer(serializers.ModelSerializer):
+    contacts = ContactSerializer(
+        many=True,
+    )
+    photos = PhotoSerializer(
+        many=True,
+    )
+
+    class Meta:
+        model = Area
+        fields = [
+            'pk',
+            'name',
+            'description',
+            'address',
+            'price',
+            'capacity',
+            'width',
+            'length',
+            'contacts',
+            'photos',
+        ]
