2017f28e2de632cb8350661a6abd789c2ff94c21
Iri O <oparina.iri@gmail.com>
2024-07-22 12:58:18 +0500

    Removed search, filter paths

--- apps/areas/urls.py
+++ apps/areas/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from areas.api import (
     AreaListView,
     AreaView,
-    AreaSearchView,
-    AreaFilterView,
 )
 
 
@@ -12,17 +10,7 @@ urlpatterns = [
     path(
         'all/',
         AreaListView.as_view(),
-        name='area_list',
-    ),
-    path(
-        'search/',
-        AreaSearchView.as_view(),
-        name='area_search',
-    ),
-    path(
-        'filter/',
-        AreaFilterView.as_view(),
-        name='area_filter',
+        name='areas',
     ),
     path(
         '<int:pk>/',
