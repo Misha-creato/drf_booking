b2a00e4e63b75130c0a7d93d8f86a3298cadd483
Iri O <oparina.iri@gmail.com>
2024-07-22 12:58:49 +0500

    Updated services

--- apps/areas/services.py
+++ apps/areas/services.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.db.models.expressions import RawSQL
 
 from areas.models import Area
@@ -8,7 +10,7 @@ from utils.logger import get_logger
 logger = get_logger(__name__)
 
 
-def get_areas() -> (int, list):
+def get_areas(request: Any, filter_backends: list, view: Any) -> (int, list):
     '''
     Получение списка всех площадок
 
@@ -34,18 +36,6 @@ def get_areas() -> (int, list):
                         "contact": "test3@cc.com",
                         "contact_type": "Email"
                     },
-                    {
-                        "contact": "wa.me/88005553535",
-                        "contact_type": "Whatsapp"
-                    },
-                    {
-                        "contact": "@test2",
-                        "contact_type": "Instagram"
-                    },
-                    {
-                        "contact": "t.me/test2",
-                        "contact_type": "Telegram"
-                    },
                 ],
                 "photos": [
                 {
@@ -61,7 +51,12 @@ def get_areas() -> (int, list):
     )
 
     try:
-        areas = Area.objects.filter(
+        areas = Area.objects.annotate(
+            price_int=RawSQL(
+                "CAST(REPLACE(price, ' ', '') AS INTEGER)",
+                []
+            )
+        ).filter(
             available=True,
         ).prefetch_related('contacts', 'photos')
     except Exception as exc:
@@ -69,6 +64,19 @@ def get_areas() -> (int, list):
             msg=f'Не удалось получить список всех площадок: {exc}',
         )
         return 500, {}
+
+    if request.query_params:
+        for backend in filter_backends:
+            try:
+                areas = backend().filter_queryset(
+                    request=request,
+                    queryset=areas,
+                    view=view,
+                )
+            except Exception as exc:
+                logger.info(
+                    msg=f'Не удалось получить список всех площадок по фильтрам: {exc}',
+                )
 
     response_data = AreaSerializer(
         instance=areas,
@@ -108,18 +116,6 @@ def get_area(pk: int) -> (int, dict):
                     "contact": "test3@cc.com",
                     "contact_type": "Email"
                 },
-                {
-                    "contact": "wa.me/88005553535",
-                    "contact_type": "Whatsapp"
-                },
-                {
-                    "contact": "@test2",
-                    "contact_type": "Instagram"
-                },
-                {
-                    "contact": "t.me/test2",
-                    "contact_type": "Telegram"
-                },
             ],
             "photos": [
                 {
@@ -158,183 +154,3 @@ def get_area(pk: int) -> (int, dict):
     )
     return 200, response_data
 
-
-def search_by_name(name: str) -> (int, list):
-    '''
-    Получение списка площадок по названию
-
-    Args:
-        name: название площадки
-
-    Returns:
-        Код статуса и список данных
-        200,
-        [
-            {
-                "pk": 1,
-                "name": "Test",
-                "description": "<p>description</p>",
-                "address": "address",
-                "price": "1000",
-                "capacity": 1000,
-                "width": 200,
-                "length": 100,
-                "contacts": [
-                    {
-                        "contact": "88005553535",
-                        "contact_type": "Phone"
-                    },
-                    {
-                        "contact": "test3@cc.com",
-                        "contact_type": "Email"
-                    },
-                    {
-                        "contact": "wa.me/88005553535",
-                        "contact_type": "Whatsapp"
-                    },
-                    {
-                        "contact": "@test2",
-                        "contact_type": "Instagram"
-                    },
-                    {
-                        "contact": "t.me/test2",
-                        "contact_type": "Telegram"
-                    },
-                ],
-                "photos": [
-                {
-                    "photo": "/media/photos/test.jpeg"
-                }
-            ]
-            },
-        ]
-    '''
-
-    logger.info(
-        msg=f'Получение списка площадок по названию {name}',
-    )
-
-    try:
-        areas = Area.objects.filter(
-            name__icontains=name,
-            available=True,
-        ).prefetch_related('contacts', 'photos')
-    except Exception as exc:
-        logger.error(
-            msg=f'Не удалось найти площадки по названию {name}: {exc}',
-        )
-        return 500, {}
-
-    response_data = AreaSerializer(
-        instance=areas,
-        many=True,
-    ).data
-    logger.info(
-        msg=f'Получен список площадок по названию {name}',
-    )
-    return 200, response_data
-
-
-def filter_by_params(params: dict) -> (int, dict):
-    '''
-    Получение списка площадок по полям
-
-    Args:
-        params: поля для фильтрации
-            {
-              "capacity": 200,
-              "min_price": 1000,
-              "max_price": 100000,
-              "width": 100,
-              "length": 100,
-            }
-
-    Returns:
-        Код статуса и список данных
-        200,
-        [
-            {
-                "pk": 1,
-                "name": "Test",
-                "description": "<p>description</p>",
-                "address": "address",
-                "price": "1000",
-                "capacity": 1000,
-                "width": 200,
-                "length": 100,
-                "contacts": [
-                    {
-                        "contact": "88005553535",
-                        "contact_type": "Phone"
-                    },
-                    {
-                        "contact": "test3@cc.com",
-                        "contact_type": "Email"
-                    },
-                    {
-                        "contact": "wa.me/88005553535",
-                        "contact_type": "Whatsapp"
-                    },
-                    {
-                        "contact": "@test2",
-                        "contact_type": "Instagram"
-                    },
-                    {
-                        "contact": "t.me/test2",
-                        "contact_type": "Telegram"
-                    },
-                ],
-                "photos": [
-                {
-                    "photo": "/media/photos/test.jpeg"
-                }
-            ]
-            },
-        ]
-    '''
-
-    logger.info(
-        msg=f'Получение списка площадок по полям {params}',
-    )
-    capacity = params.get('capacity', '0')
-    min_price = params.get('min_price', '0')
-    max_price = params.get('max_price', '1000000')
-    width = params.get('width', '0')
-    length = params.get('length', '0')
-
-    try:
-        areas = Area.objects.annotate(
-            price_int=RawSQL(
-                "CAST(REPLACE(price, ' ', '') AS INTEGER)",
-                []
-            )
-        ).exclude(
-            price='Не указана',
-        ).filter(
-            available=True,
-            capacity__gte=capacity,
-            width__gte=width,
-            length__gte=length,
-            price_int__gte=min_price,
-            price_int__lte=max_price,
-        )
-    except Exception as exc:
-        logger.error(
-            msg=f'Не удалось получить список площадок по полям {params}: {exc}',
-        )
-        return 500, {}
-
-    response_data = AreaSerializer(
-        instance=areas,
-        many=True,
-    ).data
-    logger.info(
-        msg=f'Список площадок по полям {params} получен',
-    )
-    return 200, response_data
-
-
-# def sort_by_params(params: dict) -> (int, dict)
-#     logger.info(
-#         msg=f'Сортировка списка площадок по полям {params}',
-#     )
