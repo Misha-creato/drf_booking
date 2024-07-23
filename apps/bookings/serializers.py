515f5ab6d14fe4088b45f48a6fbc5184ad9be30c
Iri O <oparina.iri@gmail.com>
2024-07-23 18:33:13 +0500

    Renamed BookingAreaSerializer, added BookingAreaSerializer

--- apps/bookings/serializers.py
+++ apps/bookings/serializers.py
@@ -3,8 +3,19 @@ from rest_framework import serializers
 from bookings.models import BookingArea
 
 
-class BookingAreaSerializer(serializers.Serializer):
+class BookAreaSerializer(serializers.Serializer):
     temporary = serializers.BooleanField()
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
 
+
+class BookingAreaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BookingArea
+        fields = [
+            'area',
+            'booked_from',
+            'booked_to',
+            'created_at'
+        ]
