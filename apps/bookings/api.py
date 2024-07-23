fa1bf5b9d8be0a2aae1ff9323519e5093070ab06
Iri O <oparina.iri@gmail.com>
2024-07-23 18:36:59 +0500

    Added BookingArea, UserBookingsConstant, UserBookingsTemporary views

---
+++ apps/bookings/api.py
@@ -0,0 +1,71 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from utils.response_patterns import generate_response
+
+from bookings.services import (
+    booking_area,
+    user_booking_constant,
+    user_booking_temporary,
+)
+
+
+class BookingAreaView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, area_pk):
+        data = request.data
+        user = request.user
+        status_code, response_data = booking_area(
+            area_pk=area_pk,
+            data=data,
+            user=user,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
+
+
+class UserBookingsConstantView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        status_code, response_data = user_booking_constant(
+            user=user,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
+
+
+class UserBookingsTemporaryView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        status_code, response_data = user_booking_temporary(
+            user=user,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
