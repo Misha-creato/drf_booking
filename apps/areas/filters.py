0ab1b839b12206050a13628bc2ac4a7b841345a2
Iri O <oparina.iri@gmail.com>
2024-07-23 18:29:34 +0500

    Updated AreaFilter

--- apps/areas/filters.py
+++ apps/areas/filters.py
@@ -1,4 +1,3 @@
-from rest_framework import filters
 import django_filters
 
 from areas.models import Area
@@ -6,44 +5,11 @@ from areas.models import Area
 
 class AreaFilter(django_filters.FilterSet):
 
-    min_capacity = django_filters.NumberFilter(
-        field_name='capacity',
-        lookup_expr='gte',
-    )
-    max_capacity = django_filters.NumberFilter(
-        field_name='capacity',
-        lookup_expr='lte',
-    )
-    min_width = django_filters.NumberFilter(
-        field_name='width',
-        lookup_expr='gte',
-    )
-    max_width = django_filters.NumberFilter(
-        field_name='width',
-        lookup_expr='lte',
-    )
-    min_length = django_filters.NumberFilter(
-        field_name='length',
-        lookup_expr='gte',
-    )
-    max_length = django_filters.NumberFilter(
-        field_name='length',
-        lookup_expr='lte',
-    )
-    min_price = django_filters.NumberFilter(
-        field_name='price_int',
-        lookup_expr='gte',
-    )
-    max_price = django_filters.NumberFilter(
-        field_name='price_int',
-        lookup_expr='lte',
-    )
-
     class Meta:
         model = Area
-        fields = [
-            'price',
-            'capacity',
-            'width',
-            'length',
-        ]
+        fields = {
+            'price': ['lte', 'gte'],
+            'capacity': ['lte', 'gte'],
+            'width': ['lte', 'gte'],
+            'length': ['lte', 'gte'],
+        }
