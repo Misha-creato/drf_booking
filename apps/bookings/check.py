0d33548a361f794eedc840082f308d9f3d14da6c
Iri O <oparina.iri@gmail.com>
2024-07-24 19:53:24 +0500

    Updated booking_dates

--- apps/bookings/check.py
+++ apps/bookings/check.py
@@ -1,13 +1,17 @@
+from datetime import datetime
 
 
 def booking_dates(constant: list, temporary: list, validated_data: dict, user_id: int) -> bool:
     start_date = validated_data['start_date']
     end_date = validated_data['end_date']
+    is_temporary = validated_data['temporary']
     if constant:
         return False
 
     for booking in temporary:
-        if booking['booked_from'] < end_date or booking['booked_to'] > start_date:
-            if booking['user_id'] != user_id:
+        booked_from = datetime.strptime(booking['booked_from'], '%Y-%m-%d %H:%M:%S%z')
+        booked_to = datetime.strptime(booking['booked_to'], '%Y-%m-%d %H:%M:%S%z')
+        if booked_from < end_date or booked_to > start_date:
+            if booking['user_id'] != user_id or is_temporary:
                 return False
     return True
