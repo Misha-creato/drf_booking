4d41181cbe4c14440b30d17aa8413f399a681777
Iri O <oparina.iri@gmail.com>
2024-07-24 18:58:18 +0500

    Updated

--- apps/areas/tests/test_services.py
+++ apps/areas/tests/test_services.py
@@ -3,6 +3,10 @@ import os
 
 from django.test import TestCase
 
+from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory
+
+from areas.api import AreaListView
 from areas.services import (
     get_areas,
     get_area,
@@ -20,9 +24,31 @@ class ServicesTest(TestCase):
         cls.path = f'{CUR_DIR}/fixtures/services'
 
     def test_get_areas(self):
-        status_code, response_data = get_areas()
-        print(response_data)
-        self.assertEqual(status_code, 200)
+        view = AreaListView
+
+        path = f'{self.path}/get_areas'
+        fixtures = (
+            (200, 'valid_ordering'),
+            (200, 'valid_search'),
+            (200, 'valid_filters'),
+        )
+
+        for code, name in fixtures:
+            fixture = f'{code}_{name}'
+
+            with open(f'{path}/{fixture}_request.json') as file:
+                data = json.load(file)
+
+            factory = APIRequestFactory()
+            request = factory.get('/', data)
+            request = Request(request)
+            status_code, response_data = get_areas(
+                request=request,
+                filter_backends=view.filter_backends,
+                view=view,
+            )
+            print(response_data)
+            self.assertEqual(status_code, status_code, msg=fixture)
 
     def test_get_area(self):
         path = f'{self.path}/get_area'
