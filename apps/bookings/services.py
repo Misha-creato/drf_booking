1686a53f972fe321b99b1dce5a85fb0c40b2da82
Iri O <oparina.iri@gmail.com>
2024-07-25 18:04:14 +0500

    Added get_area_booking_temporary, get_area_qr_data, get_area_qr_data

--- apps/bookings/services.py
+++ apps/bookings/services.py
@@ -1,7 +1,12 @@
+import uuid
+
 from django.http import QueryDict
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 from areas.models import Area
+
+from utils import redis_cache
 from utils.logger import get_logger
 
 from bookings import check
@@ -9,11 +14,8 @@ from bookings.models import BookingArea
 from bookings.serializers import (
     BookAreaSerializer,
     BookingAreaSerializer,
-)
-
-from utils.project_redis import (
-    get_temporary_bookings_by_key,
-    set_temporary_booking,
+    GenerateQRSerializer,
+    BookingAreaQRSerializer,
 )
 
 
@@ -25,7 +27,7 @@ def booking_area(area_pk: int, data: QueryDict, user: User) -> (int, dict):
     temporary = data.get('temporary')
     logger.info(
         msg=f'Бронирование площадки {area_pk} пользователем {user}, '
-            f'временная бронь: {temporary}'
+            f'(временная бронь - {temporary})'
     )
 
     try:
@@ -36,13 +38,15 @@ def booking_area(area_pk: int, data: QueryDict, user: User) -> (int, dict):
     except Exception as exc:
         logger.error(
             msg=f'Возникла ошибка при бронировании площадки {area_pk} '
-                f'пользователем {user}: {exc}'
+                f'пользователем {user} (временная бронь - {temporary}): '
+                f'{exc}'
         )
         return 500, {}
 
     if area is None:
         logger.error(
-            msg=f'Площадка для бронирования {area_pk} пользователем {user} '
+            msg=f'Площадка для бронирования {area_pk} '
+                f'пользователем {user} (временная бронь - {temporary}) '
                 f'не найдена',
         )
         return 404, {}
@@ -53,7 +57,8 @@ def booking_area(area_pk: int, data: QueryDict, user: User) -> (int, dict):
     if not serializer.is_valid():
         logger.error(
             msg=f'Невалидные данные для бронирования площадки {area_pk} '
-                f'пользователем {user}: {serializer.errors}',
+                f'пользователем {user} (временная бронь - {temporary}): '
+                f'{serializer.errors}',
         )
         return 400, {}
 
@@ -67,14 +72,20 @@ def booking_area(area_pk: int, data: QueryDict, user: User) -> (int, dict):
     except Exception as exc:
         logger.error(
             msg=f'Возникла ошибка при бронировании площадки {area_pk} '
-                f'пользователем {user}: {exc}',
+                f'пользователем {user} (временная бронь - {temporary}): '
+                f'{exc}',
         )
         return 500, {}
 
-    key_pattern = f'*area{area_pk}*'
-    status, temporary_bookings = get_temporary_bookings_by_key(
-        key_pattern=key_pattern,
+    status, temporary_bookings = get_area_booking_temporary(
+        area_pk=area_pk,
     )
+    if status != 200:
+        logger.error(
+            msg=f'Не удалось забронировать площадку {area_pk} '
+                f'пользователем {user} (временная бронь - {temporary})',
+        )
+        return status, {}
 
     valid_booking_dates = check.booking_dates(
         constant=constant_bookings,
@@ -85,64 +96,53 @@ def booking_area(area_pk: int, data: QueryDict, user: User) -> (int, dict):
     if not valid_booking_dates:
         logger.error(
             msg=f'Не удалось забронировать площадку {area_pk} '
-                f'пользователем {user}. Даты бронирования заняты'
+                f'пользователем {user} (временная бронь - {temporary}): '
+                f'даты бронирования заняты'
         )
         return 400, {}
 
-    return book_area_by_status(
-        area_pk=area_pk,
-        validated_data=validated_data,
-        user=user,
-    )
-
-
-def book_area_by_status(area_pk: int, validated_data: dict, user: User) -> (int, dict):
     start_date = validated_data['start_date']
     end_date = validated_data['end_date']
-    if validated_data['temporary']:
-        logger.info(
-            msg=f'Временное бронирование площадки {area_pk} '
-                f'пользователем {user}',
-        )
-        status, response = set_temporary_booking(
-            area_pk=area_pk,
-            validated_data=validated_data,
-            user=user,
+    if not temporary:
+        try:
+            BookingArea.objects.create(
+                area_id=area_pk,
+                user=user,
+                booked_from=start_date,
+                booked_to=end_date,
+            )
+        except Exception as exc:
+            logger.error(
+                msg=f'Возникла ошибка при бронировании площадки {area_pk} '
+                    f'пользователем {user} (временная бронь - {temporary}): '
+                    f'{exc}',
+            )
+            return 500, {}
+    else:
+        key = f'area{area_pk}_user{user.id}_{str(uuid.uuid4())}'
+        data = {
+            'area': area_pk,
+            'booked_from': start_date.strftime('%Y-%m-%d %H:%M:%S%z'),
+            'booked_to': end_date.strftime('%Y-%m-%d %H:%M:%S%z'),
+            'user_id': user.id,
+            'created_at': timezone.now().strftime('%Y-%m-%d %H:%M:%S%z'),
+        }
+        status = redis_cache.set_key(
+            key=key,
+            data=data,
+            time=60 * 60,
         )
         if status != 200:
             logger.error(
                 msg=f'Не удалось временно забронировать площадку {area_pk} '
-                    f'пользователем {user}',
+                    f'пользователем {user} (временная бронь - {temporary})',
             )
             return status, {}
 
-        logger.info(
-            msg=f'Временное бронирование площадки {area_pk} '
-                f'пользователем {user} прошло успешно',
-        )
-        return 200, {}
-
     logger.info(
-        msg=f'Постоянное бронирование площадки {area_pk} '
-            f'пользователем {user}',
-    )
-    try:
-        booking = BookingArea.objects.create(
-            area_id=area_pk,
-            user=user,
-            booked_from=start_date,
-            booked_to=end_date,
-        )
-    except Exception as exc:
-        logger.error(
-            msg=f'Возникла ошибка при постоянном бронировании площадки {area_pk} '
-                f'пользователем {user}: {exc}',
-        )
-        return 500, {}
-
-    logger.info(
-        msg=f'Постоянное бронирование площадки {area_pk} '
-            f'пользователем {user} прошло успешно',
+        msg=f'Бронирование площадки {area_pk} '
+            f'пользователем {user} (временная бронь - {temporary}) '
+            f'прошло успешно',
     )
     return 200, {}
 
@@ -175,13 +175,13 @@ def user_booking_history(user: User) -> (int, list):
     return 200, response_data
 
 
-def user_booking_temporary(user: User) -> (int, dict):
+def user_booking_temporary(user: User) -> (int, list):
     logger.info(
         msg=f'Получение списка временных броней пользователя {user}',
     )
 
     key_pattern = f'*_user{user.id}_*'
-    status, response_data = get_temporary_bookings_by_key(
+    status, matching_keys = redis_cache.get_matching_keys(
         key_pattern=key_pattern,
     )
     if status != 200:
@@ -189,9 +189,147 @@ def user_booking_temporary(user: User) -> (int, dict):
             msg=f'Не удалось получить список временных броней '
                 f'пользователя {user}',
         )
-        return status
+        return status, []
+
+    response_data = []
+    for key in matching_keys:
+        status, data = redis_cache.get(
+            key=key,
+        )
+        if status != 200:
+            return status, []
+
+        response_data.append(data)
 
     logger.info(
         msg=f'Получен список временных броней пользователя {user}',
     )
     return 200, response_data
+
+
+def get_area_booking_temporary(area_pk: int) -> (int, list):
+    logger.info(
+        msg=f'Получение списка временных броней площадки {area_pk}',
+    )
+
+    key_pattern = f'*area{area_pk}*'
+    status, matching_keys = redis_cache.get_matching_keys(
+        key_pattern=key_pattern,
+    )
+    if status != 200:
+        logger.error(
+            msg=f'Не удалось получить список временных броней '
+                f'площадки {area_pk}',
+        )
+        return status, []
+
+    response_data = []
+    for key in matching_keys:
+        status, data = redis_cache.get(
+            key=key,
+        )
+        if status != 200:
+            return status, []
+
+        response_data.append(data)
+
+    logger.info(
+        msg=f'Получен список временных броней площадки {area_pk}',
+    )
+    return 200, response_data
+
+
+def get_area_qr_data(data: QueryDict) -> (int, dict):
+    logger.info(
+        msg=f'Получение данных для генерации qr для '
+            f'бронирования {data}',
+    )
+
+    serializer = GenerateQRSerializer(
+        data=data,
+    )
+    if not serializer.is_valid():
+        logger.error(
+            msg=f'Невалидные данные для генерации QR для '
+                f'бронирования {data}: {serializer.errors}',
+        )
+        return 400, {}
+
+    validated_data = serializer.validated_data
+    try:
+        booking = BookingArea.objects.filter(
+            id=validated_data['booking_id'],
+            booked_to__gt=timezone.now()
+        ).first()
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при получении данных '
+                f'для генерации QR для бронирования {data}: {exc}',
+        )
+        return 500, {}
+
+    if booking is None:
+        logger.error(
+            msg=f'Бронирование {data} при получении данных '
+                f'для генерации QR не найдено',
+        )
+        return 400, {}
+
+    response_data = BookingAreaQRSerializer(
+        instance=booking,
+    ).data
+    logger.info(
+        msg=f'Получены данные для генерации QR для бронирования {data}',
+    )
+    return 200, response_data
+
+
+def area_qr_check(data: QueryDict) -> (int, dict):
+    logger.info(
+        msg=f'Подтверждение начала бронирования {data}',
+    )
+
+    serializer = BookingAreaQRSerializer(
+        data=data,
+    )
+    if not serializer.is_valid():
+        logger.error(
+            msg=f'Невалидные данные для подтверждения '
+                f'начала бронирования {data}: {serializer.errors}',
+        )
+        return 400, {}
+
+    validated_data = serializer.validated_data
+    try:
+        booking = BookingArea.objects.filter(
+            id=validated_data['id'],
+            booked_to__gt=timezone.now(),
+        ).first()
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при подтверждении начала '
+                f'бронирования {data}: {exc}',
+        )
+        return 500, {}
+
+    if booking is None:
+        logger.error(
+            msg=f'Бронирование {data} для подтверждения '
+                f'начала не найдено',
+        )
+        return 400, {}
+
+    booking.started = True
+    try:
+        booking.save()
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при подтверждении начала '
+                f'бронирования {data}: {exc}',
+        )
+        return 500, {}
+
+    logger.info(
+        msg=f'Успешно подтверждено начало бронирования {data}',
+    )
+    return 200, {}
