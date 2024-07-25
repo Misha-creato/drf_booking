3c56ef0d35b1e80749c9fe1d80f4d589d0162a3a
Iri O <oparina.iri@gmail.com>
2024-07-25 18:02:01 +0500

    Removed get_temporary, set_temporary, added get, set_key, get_keys

--- utils/redis_cache.py
+++ utils/redis_cache.py
@@ -1,10 +1,8 @@
 import json
-import uuid
-
 import redis
+from typing import Any
 
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 
 from config.settings import (
     REDIS_HOST,
@@ -19,56 +17,67 @@ logger = get_logger(__name__)
 redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=1)
 
 
-def get_temporary_bookings_by_key(key_pattern: str) -> (int, list):
+def set_key(key: str, data: Any, time: int = None) -> int:
     logger.info(
-        msg=f'Получение списка временных броней по шаблону ключа {key_pattern}',
+        msg=f'Добавление данных {data} в redis по ключу {key}',
     )
+
+    data_json = json.dumps(obj=data)
     try:
-        matching_keys = redis_client.keys(key_pattern)
-        temporary_bookings = [json.loads(redis_client.get(key)) for key in matching_keys]
+        if time is None:
+            redis_client.set(name=key, value=data_json)
+        else:
+            redis_client.setex(name=key, time=time, value=data_json)
     except Exception as exc:
         logger.error(
-            msg=f'Возникла ошибка при получении временных броней '
-                f'по шаблону ключа {key_pattern}: {exc}',
+            msg=f'Возникла ошибка при добавлении данных {data} '
+                f'в redis по ключу {key}: {exc}',
+        )
+        return 500
+
+    logger.info(
+        msg=f'Успешно добавлены данные {data} в redis по ключу {key}',
+    )
+    return 200
+
+
+def get(key: str) -> (int, Any):
+    logger.info(
+        msg=f'Получение данных из redis по ключу {key}',
+    )
+
+    try:
+        data = redis_client.get(name=key)
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при получении данных из redis '
+                f'по ключу {key}: {exc}',
+        )
+        return 500, None
+
+    logger.info(
+        msg=f'Успешно получены данные из redis по ключу {key}'
+    )
+    return 200, json.loads(s=data)
+
+
+def get_matching_keys(key_pattern: str) -> (int, list):
+    logger.info(
+        msg=f'Получение списка подходящих ключей из redis '
+            f'по шаблону {key_pattern}',
+    )
+
+    try:
+        matching_keys = redis_client.keys(key_pattern)
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при получении списка подходящих '
+                f'ключей из redis по шаблону {key_pattern}: {exc}',
         )
         return 500, []
 
     logger.info(
-        msg=f'Получен список временных броней по шаблону ключа {key_pattern}',
+        msg=f'Получен список подходящих ключей из redis '
+            f'по шаблону {key_pattern}',
     )
-    return 200, temporary_bookings
-
-
-def set_temporary_booking(area_pk: int, validated_data: dict, user: User) -> (int, {}):
-    start_date = validated_data['start_date']
-    end_date = validated_data['end_date']
-    data = {
-        'area': area_pk,
-        'booked_from': start_date.strftime('%Y-%m-%d %H:%M:%S%z'),
-        'booked_to': end_date.strftime('%Y-%m-%d %H:%M:%S%z'),
-        'user_id': user.id,
-        'created_at': timezone.now().strftime('%Y-%m-%d %H:%M:%S%z'),
-    }
-    key = f'area{area_pk}_user{user.id}_{str(uuid.uuid4())}'
-    try:
-        key_data = json.dumps(data)
-        redis_client.setex(name=key, time=60, value=key_data)
-    except Exception as exc:
-        logger.error(
-            msg=f'Возникла ошибка при временном бронировании площадки {area_pk} '
-                f'пользователем {user}: {exc}',
-        )
-        return 500, {}
-
-    return 200, {}
-
-
-def set_email_settings(email_settings: dict) -> None:
-    email_settings_json = json.dumps(email_settings)
-    redis_client.set('email_settings', email_settings_json)
-
-
-def get_email_settings() -> dict:
-    email_settings = redis_client.get('email_settings')
-    return json.loads(email_settings)
-
+    return 200, matching_keys
