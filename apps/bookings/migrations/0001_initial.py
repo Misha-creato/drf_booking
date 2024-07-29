2ebaf85be0794f9de47c5bb15a2158ac6b361488
Iri O <oparina.iri@gmail.com>
2024-07-29 19:18:46 +0500

    Updated migrations

--- apps/bookings/migrations/0001_initial.py
+++ apps/bookings/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('booked_from', models.DateTimeField(verbose_name='От')),
                 ('booked_to', models.DateTimeField(verbose_name='До')),
                 ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата бронирования')),
-                ('area', models.ForeignKey(default='Площадка удалена', on_delete=django.db.models.deletion.SET_DEFAULT, related_name='bookings', to='areas.area', verbose_name='Площадка')),
+                ('area', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='bookings', to='areas.area', verbose_name='Площадка')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='area_bookings', to=settings.AUTH_USER_MODEL, verbose_name='Пользователь')),
             ],
             options={
