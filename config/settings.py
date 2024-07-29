8477196a0e7f26682883e8899beb7c038a98dbc2
Iri O <oparina.iri@gmail.com>
2024-07-29 19:21:57 +0500

    Added allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,9 @@ DEBUG = os.environ.get(
 )
 DEBUG = DEBUG == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', '127.0.0.1'
+).split(',')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
