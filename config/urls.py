7c5706a0ed01769695415a1f3f633271c87190fd
Iri O <oparina.iri@gmail.com>
2024-08-05 19:19:46 +0500

    Updated urls

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import (
     path,
@@ -33,10 +34,11 @@ urlpatterns = [
     path('api/v1/users/', include('users.urls')),
     path('api/v1/areas/', include('areas.urls')),
     path('api/v1/bookings/', include('bookings.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
         path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
         path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
