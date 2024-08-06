9d2841901567c39ac51ac8e403ad3766be090b76
Iri O <oparina.iri@gmail.com>
2024-08-06 19:31:39 +0500

    Updated static root

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get(
 DEBUG = DEBUG == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', 'localhost, 127.0.0.1, 127.0.0.1:80'
+    'ALLOWED_HOSTS', 'localhost, 127.0.0.1'
 ).replace(' ', '').replace('|', '').split(',')
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -43,11 +43,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
-    'http://127.0.0.1:80',
 ]
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1',
-    'http://127.0.0.1:80',
 ]
 
 # Application definition
@@ -218,15 +216,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'var/www/staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'var/www/static')
 
 # Media
 
-MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
